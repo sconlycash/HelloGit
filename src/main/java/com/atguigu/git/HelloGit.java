96b6178539cd5d6cde81a2ef9a26de2de57e662d
syc123 <994920920@qq.com>
2023-01-18 15:39:10 +0800

    创建分支

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hellogit!");
         System.out.println("hellogit2!");
+        System.out.println("hellogit2!");
     }
 }
