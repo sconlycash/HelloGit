5a0bbcfd09901cea720e9c66b61168c3365ff5dd
syc123 <994920920@qq.com>
2023-01-18 15:32:25 +0800

    更新1

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -4,5 +4,6 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hellogit!");
+        System.out.println("hellogit2!");
     }
 }
